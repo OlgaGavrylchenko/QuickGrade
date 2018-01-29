2a4834032b25b36dacce2fbd9a35e1fc400467a0
OlgaGavrylchenko <15853578+OlgaGavrylchenko@users.noreply.github.com>
2018-01-29 14:41:17 -0500

    Add files via upload

---
+++ GradingApp/GradingApp/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace GradingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.tfFinal = new System.Windows.Forms.TextBox();
+            this.tfExam = new System.Windows.Forms.TextBox();
+            this.tfProjectThree = new System.Windows.Forms.TextBox();
+            this.tfProjectTwo = new System.Windows.Forms.TextBox();
+            this.tfProjectOne = new System.Windows.Forms.TextBox();
+            this.tfParticipation = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tfQulity = new System.Windows.Forms.TextBox();
+            this.tfGrade = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tfFinal);
+            this.panel1.Controls.Add(this.tfExam);
+            this.panel1.Controls.Add(this.tfProjectThree);
+            this.panel1.Controls.Add(this.tfProjectTwo);
+            this.panel1.Controls.Add(this.tfProjectOne);
+            this.panel1.Controls.Add(this.tfParticipation);
+            this.panel1.Controls.Add(this.label14);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(376, 288);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(48, 445);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(110, 30);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // tfFinal
+            // 
+            this.tfFinal.Location = new System.Drawing.Point(187, 235);
+            this.tfFinal.Name = "tfFinal";
+            this.tfFinal.Size = new System.Drawing.Size(140, 25);
+            this.tfFinal.TabIndex = 5;
+            // 
+            // tfExam
+            // 
+            this.tfExam.Location = new System.Drawing.Point(187, 197);
+            this.tfExam.Name = "tfExam";
+            this.tfExam.Size = new System.Drawing.Size(140, 25);
+            this.tfExam.TabIndex = 4;
+            // 
+            // tfProjectThree
+            // 
+            this.tfProjectThree.Location = new System.Drawing.Point(187, 158);
+            this.tfProjectThree.Name = "tfProjectThree";
+            this.tfProjectThree.Size = new System.Drawing.Size(140, 25);
+            this.tfProjectThree.TabIndex = 3;
+            // 
+            // tfProjectTwo
+            // 
+            this.tfProjectTwo.Location = new System.Drawing.Point(187, 118);
+            this.tfProjectTwo.Name = "tfProjectTwo";
+            this.tfProjectTwo.Size = new System.Drawing.Size(140, 25);
+            this.tfProjectTwo.TabIndex = 2;
+            // 
+            // tfProjectOne
+            // 
+            this.tfProjectOne.Location = new System.Drawing.Point(187, 77);
+            this.tfProjectOne.Name = "tfProjectOne";
+            this.tfProjectOne.Size = new System.Drawing.Size(140, 25);
+            this.tfProjectOne.TabIndex = 1;
+            // 
+            // tfParticipation
+            // 
+            this.tfParticipation.Location = new System.Drawing.Point(187, 35);
+            this.tfParticipation.Name = "tfParticipation";
+            this.tfParticipation.Size = new System.Drawing.Size(140, 25);
+            this.tfParticipation.TabIndex = 0;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(19, 235);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(137, 17);
+            this.label14.TabIndex = 5;
+            this.label14.Text = "Final Exam Project:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(19, 197);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(112, 17);
+            this.label13.TabIndex = 4;
+            this.label13.Text = "Midterm Exam:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(19, 158);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(80, 17);
+            this.label12.TabIndex = 3;
+            this.label12.Text = "Project #3:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(19, 118);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(80, 17);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "Project #2:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(19, 77);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(80, 17);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Project #1:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(19, 35);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(92, 17);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Attendance: ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tfQulity);
+            this.groupBox3.Controls.Add(this.tfGrade);
+            this.groupBox3.Controls.Add(this.label16);
+            this.groupBox3.Controls.Add(this.label15);
+            this.groupBox3.Location = new System.Drawing.Point(14, 316);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(374, 123);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Result";
+            // 
+            // tfQulity
+            // 
+            this.tfQulity.Location = new System.Drawing.Point(115, 75);
+            this.tfQulity.Name = "tfQulity";
+            this.tfQulity.ReadOnly = true;
+            this.tfQulity.Size = new System.Drawing.Size(140, 25);
+            this.tfQulity.TabIndex = 13;
+            this.tfQulity.TabStop = false;
+            // 
+            // tfGrade
+            // 
+            this.tfGrade.Location = new System.Drawing.Point(115, 34);
+            this.tfGrade.Name = "tfGrade";
+            this.tfGrade.ReadOnly = true;
+            this.tfGrade.Size = new System.Drawing.Size(140, 25);
+            this.tfGrade.TabIndex = 12;
+            this.tfGrade.TabStop = false;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(21, 75);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(60, 17);
+            this.label16.TabIndex = 1;
+            this.label16.Text = "Quality:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(21, 34);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(53, 17);
+            this.label15.TabIndex = 0;
+            this.label15.Text = "Grade:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnSubmit.Location = new System.Drawing.Point(164, 445);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(110, 30);
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "CALCULATE";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 30);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "EXIT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(402, 486);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QuickGrade";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tfFinal;
+        private System.Windows.Forms.TextBox tfExam;
+        private System.Windows.Forms.TextBox tfProjectThree;
+        private System.Windows.Forms.TextBox tfProjectTwo;
+        private System.Windows.Forms.TextBox tfProjectOne;
+        private System.Windows.Forms.TextBox tfParticipation;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox tfQulity;
+        private System.Windows.Forms.TextBox tfGrade;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GradingApp/GradingApp/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace GradingApp
+{
+    public partial class Form1 : Form
+    {
+        private string participation;
+        private string projectOne;
+        private string projectTwo;
+        private string projectThree;
+        private string midtermExam;
+        private string finalExam;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            this.tfParticipation.Focus();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //assign text fields to empty string
+            this.tfParticipation.Text = "";
+            this.tfProjectOne.Text = "";
+            this.tfProjectTwo.Text = "";
+            this.tfProjectThree.Text = "";
+            this.tfExam.Text = "";
+            this.tfFinal.Text = "";
+
+            this.tfParticipation.Focus();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(this.tfParticipation.Text) ||
+                String.IsNullOrEmpty(this.tfProjectOne.Text) ||
+                String.IsNullOrEmpty(this.tfProjectTwo.Text) ||
+                String.IsNullOrEmpty(this.tfProjectThree.Text) ||
+                String.IsNullOrEmpty(this.tfExam.Text) ||
+                String.IsNullOrEmpty(this.tfFinal.Text))
+            {
+                MessageBox.Show("Text fields are empty", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                this.tfParticipation.Focus();
+            }
+            else {
+
+                participation = this.tfParticipation.Text;
+                projectOne = this.tfProjectOne.Text;
+                projectTwo = this.tfProjectTwo.Text;
+                projectThree = this.tfProjectThree.Text;
+                midtermExam = this.tfExam.Text;
+                finalExam = this.tfFinal.Text;
+
+                if (isValid(participation) && isValid(projectOne) && isValid(projectTwo) &&
+                    isValid(projectThree) && isValid(midtermExam) && isValid(finalExam))
+                {
+                    double x1 = Convert.ToDouble(participation);
+                    double x2 = Convert.ToDouble(projectOne);
+                    double x3 = Convert.ToDouble(projectTwo);
+                    double x4 = Convert.ToDouble(projectThree);
+                    double x5 = Convert.ToDouble(midtermExam);
+                    double x6 = Convert.ToDouble(finalExam);
+
+                    double result = getResultNumber(x1, x2, x3, x4, x5, x6);
+
+                    if (result > 100)
+                    {
+                        MessageBox.Show("Data is too big or invalid", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        this.tfParticipation.Focus();
+                    }
+                    else {
+                        string resultL = getResultLetter(result);
+
+                        this.tfGrade.Text = result.ToString("f2"); //to digits after point
+                        this.tfQulity.Text = resultL;
+                    }  
+                }
+                else
+                {
+                    MessageBox.Show("Data is invalid", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    this.tfParticipation.Focus();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.DialogResult result;
+            result = MessageBox.Show("Are you sure?", "Confiramtion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                //close form
+                this.Close();
+            }
+            else {
+                this.tfParticipation.Focus();    
+            }
+        }
+
+        private double getResultNumber(double attend, double pOne, double pTwo, double pThree, double exam, double fExam) {
+
+            double result = 0.0;
+
+            result = (attend * 0.10) + (pOne * 0.15) + (pTwo * 0.15) + (pThree * 0.15) + (exam * 0.20) + (fExam * 0.25);
+
+            return result;
+        }
+
+        private string getResultLetter(double data) {
+            string result = "";
+
+            if (data <= 100 && data >= 94)
+            {
+                result = "A";
+            }
+            else if (data < 94 && data >= 90)
+            {
+                result = "A-";
+            }
+            else if (data < 90 && data >= 87)
+            {
+                result = "B+";
+            }
+            else if (data < 87 && data >= 83)
+            {
+                result = "B";
+            }
+            else if (data < 83 && data >= 80)
+            {
+                result = "B-";
+            }
+            else if (data < 80 && data >= 77)
+            {
+                result = "C+";
+            }
+            else if (data < 77 && data >= 70)
+            {
+                result = "C";
+            }
+            else if (data < 70 && data >= 60)
+            {
+                result = "D";
+            }
+            else if (data < 60)
+            {
+                result = "F";
+            }
+
+            return result;
+        }
+
+        private bool isValid(string str) {
+            return Regex.IsMatch(str, @"^[0-9]+$");
+        }
+    }
+}
